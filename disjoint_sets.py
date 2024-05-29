a4affd1eeaadbbb5ea79061b069869cfcc51e5c2
Khalid <Khalidbelhadj124@gmail.com>
2024-05-29 09:42:40 +0100

    Reimplement disjoint sets to use recursive Node class

--- disjoint_sets.py
+++ disjoint_sets.py
@@ -1,12 +1,10 @@
-from typing import TypedDict
-
-
-class Node(TypedDict):
+class Node:
     """Helper class to represent a node in DisjointSets"""
 
-    next: int | None
-    value: int
-    parent: int
+    def __init__(self, value: int):
+        self.value = value
+        self.next: Node | None = None
+        self.parent: Node | None = None
 
 
 class DisjointSets:
@@ -15,23 +13,20 @@ class DisjointSets:
     def __init__(self):
         self.nodes: list[Node] = []
 
+    def get_node(self, a: int) -> Node:
+        return next(x for x in self.nodes if x.value == a)
+
     def make_set(self, a: int):
         """Creates a set with a single element `a`"""
-        self.nodes.append(
-            {
-                "next": None,
-                "parent": a,
-                "value": a,
-            }
-        )
+        node = Node(a)
+        node.parent = node
+        self.nodes.append(node)
 
-    def find_set(self, a: int) -> int | None:
+    def find_set(self, a: Node) -> Node | None:
         """Finds the representative of the set which contains `a`"""
-        for node in self.nodes:
-            if node["value"] == a:
-                return node["parent"]
+        return a.parent
 
-    def union(self, a: int, b: int):
+    def union(self, a: Node, b: Node):
         """Joints the two sets containing `a` and `b`"""
         parent_a = self.find_set(a)
         parent_b = self.find_set(b)
@@ -39,6 +34,9 @@ class DisjointSets:
         if parent_a is None or parent_b is None:
             return
 
-        for node in self.nodes:
-            if node["parent"] == parent_a:
-                node["parent"] = parent_b
+        current = parent_a
+        while current.next is not None:
+            current.parent = parent_b
+            current = current.next
+        current.parent = parent_b
+        current.next = b
